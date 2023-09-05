c6cb3799653d2ea0aa0e3526012e30b7bc834d55
Pragati Rode <pragatirode@Pragatis-MacBook-Air.local>
2023-09-05 15:08:23 +0530

    Initial Commit

---
+++ MVVMSwiftUICombine/MVVMSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUICombineApp.swift
+//  MVVMSwiftUICombine
+//
+//  Created by Pragati Rode on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
